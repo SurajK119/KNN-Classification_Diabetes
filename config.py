b4667da3bc94cad691cd6d29faa2b894b839e4a9
Your Name <you@example.com>
2023-04-11 19:55:55 +0530

    Configuration file of this project

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+MODEL_FILE_PATH = os.path.join("project_app", "Diabetes_Classification.pkl")
+
+NORMAL_SCALER_FILE_PATH = os.path.join("project_app", "Normalization.pkl")
+
+JSON_FILE_PATH = os.path.join("project_app", "project_data.json")
+
+PORT_NUMBER = 5005
